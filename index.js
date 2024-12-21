8e1ba0dccebd86cd04983d32971a3e341b924f7e
s0ulcats <babich.danya30@gmail.com>
2024-12-21 19:36:49 +0200

    dsa

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const DB_USER = 'tbeteam229';
 const DB_PASSWORD = 'yQ4fnXTcCUr4x40J';
 const DB_NAME = 'tbepanel';
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.static('uploads'));
