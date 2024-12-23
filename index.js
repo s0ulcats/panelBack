973083173712bf1637fbb4314ba903910d3dcf46
s0ulcats <babich.danya30@gmail.com>
2024-12-23 11:41:36 +0200

    sadf

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
-const https = require('https');  // Подключаем модуль для работы с HTTPS
-const fs = require('fs');        // Для чтения сертификатов
 const authRoute = require('./routes/auth.js');
 const usersRoute = require('./routes/users.js');
 
@@ -16,12 +14,8 @@ const DB_USER = 'tbeteam229';
 const DB_PASSWORD = 'yQ4fnXTcCUr4x40J';
 const DB_NAME = 'tbepanel';
 
-// Чтение SSL сертификатов
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/your-domain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/your-domain.com/fullchain.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
 app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'https://tbe-faceit.online' }));
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.static('uploads'));
@@ -36,9 +30,8 @@ async function start() {
         );
         console.log('Connected to DB');
 
-        // Запуск HTTPS сервера
-        https.createServer(credentials, app).listen(PORT, () => {
-            console.log(`Server started on https://localhost:${PORT}`);
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
         });
     } catch (error) {
         console.log('DB connection error:', error);
